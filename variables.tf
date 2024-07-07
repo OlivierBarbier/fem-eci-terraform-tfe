c298d344ed93e31b86b50a434ca45289ce1ce158
Olivier BARBIER <olivierb@clipr.co>
2024-07-07 09:51:35 +0200

    feat: setup vcs automation

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "skale"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = "52565401"
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "OlivierBarbier"
+  type    = string
+}
